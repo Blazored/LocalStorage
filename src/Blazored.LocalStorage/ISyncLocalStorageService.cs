9f15dec3c88ba272859d9e1f56238caa13d48fe4
Kirk Woll <kirk.woll@gmail.com>
2019-03-09 06:13:52 -0800

    Add support for non-async calls

---
+++ src/Blazored.LocalStorage/ISyncLocalStorageService.cs
@@ -0,0 +1,17 @@
+﻿namespace Blazored.LocalStorage
+{
+    public interface ISyncLocalStorageService
+    {
+        void Clear();
+
+        T GetItem<T>(string key);
+
+        string Key(int index);
+
+        int Length();
+
+        void RemoveItem(string key);
+
+        void SetItem(string key, object data);
+    }
+}
