0eaeeae507f99333b14707860be1d08c955c635b
Chris Sainty <chrissainty@users.noreply.github.com>
2019-03-26 22:35:44 +0000

    Fixed some method names and bumped version

--- src/Blazored.LocalStorage/ChangedEventArgs.cs
+++ src/Blazored.LocalStorage/ChangedEventArgs.cs
@@ -3,9 +3,6 @@
     public class ChangedEventArgs
     {
         public string Key { get; set; }
-        
-        // NOTE: can't easily make event handlers generic
-
         public object OldValue { get; set; }
         public object NewValue { get; set; }
     }
--- src/Blazored.LocalStorage/LocalStorageService.cs
+++ src/Blazored.LocalStorage/LocalStorageService.cs
@@ -57,10 +57,11 @@ namespace Blazored.LocalStorage
 
         public async Task<string> KeyAsync(int index) => await _jSRuntime.InvokeAsync<string>("Blazored.LocalStorage.Key", index);
 
-        void ISyncLocalStorageService.SetItem(string key, object data)
+        public void SetItem(string key, object data)
         {
             if (string.IsNullOrEmpty(key))
                 throw new ArgumentNullException(nameof(key));
+
             if (_jSInProcessRuntime == null)
                 throw new InvalidOperationException("IJSInProcessRuntime not available");
 
@@ -74,10 +75,11 @@ namespace Blazored.LocalStorage
             RaiseOnChanged(key, e.OldValue, data);
         }
 
-        T ISyncLocalStorageService.GetItem<T>(string key)
+        public T GetItem<T>(string key)
         {
             if (string.IsNullOrEmpty(key))
                 throw new ArgumentNullException(nameof(key));
+
             if (_jSInProcessRuntime == null)
                 throw new InvalidOperationException("IJSInProcessRuntime not available");
 
@@ -89,17 +91,18 @@ namespace Blazored.LocalStorage
             return Json.Deserialize<T>(serialisedData);
         }
 
-        void ISyncLocalStorageService.RemoveItem(string key)
+        public void RemoveItem(string key)
         {
             if (string.IsNullOrEmpty(key))
                 throw new ArgumentNullException(nameof(key));
+                
             if (_jSInProcessRuntime == null)
                 throw new InvalidOperationException("IJSInProcessRuntime not available");
 
             _jSInProcessRuntime.Invoke<object>("Blazored.LocalStorage.RemoveItem", key);
         }
 
-        void ISyncLocalStorageService.Clear()
+        public void Clear()
         {
             if (_jSInProcessRuntime == null)
                 throw new InvalidOperationException("IJSInProcessRuntime not available");
@@ -107,7 +110,7 @@ namespace Blazored.LocalStorage
             _jSInProcessRuntime.Invoke<object>("Blazored.LocalStorage.Clear");
         }
 
-        int ISyncLocalStorageService.Length()
+        public int Length()
         {
             if (_jSInProcessRuntime == null)
                 throw new InvalidOperationException("IJSInProcessRuntime not available");
@@ -115,7 +118,7 @@ namespace Blazored.LocalStorage
             return _jSInProcessRuntime.Invoke<int>("Blazored.LocalStorage.Length");
         }
 
-        string ISyncLocalStorageService.Key(int index)
+        public string Key(int index)
         {
             if (_jSInProcessRuntime == null)
                 throw new InvalidOperationException("IJSInProcessRuntime not available");
