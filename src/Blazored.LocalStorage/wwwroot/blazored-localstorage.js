e252727846715aed0c15ab3edeed79adc3343289
Chris Sainty <chrissainty@users.noreply.github.com>
2019-08-14 11:05:33 +0100

    Fixed gitignore

---
+++ src/Blazored.LocalStorage/wwwroot/blazored-localstorage.js
@@ -0,0 +1,20 @@
+﻿window.blazoredLocalStorage = {
+    setItem: function (key, data) {
+        localStorage.setItem(key, data);
+    },
+    getItem: function (key) {
+        return localStorage.getItem(key);
+    },
+    removeItem: function (key) {
+        localStorage.removeItem(key);
+    },
+    clear: function () {
+        localStorage.clear();
+    },
+    length: function () {
+        return localStorage.length;
+    },
+    key: function (index) {
+        return localStorage.key(index);
+    }
+};
