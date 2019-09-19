402f30ad34f5e14a77e0d2839eaf91c90985a466
julienGrd <jul.gourdon@gmail.com>
2019-09-19 14:45:30 +0200

    Update src/Blazored.LocalStorage/ISyncLocalStorageService.cs
    correct comments
    
    Co-Authored-By: Chris Sainty <chrissainty@users.noreply.github.com>

--- src/Blazored.LocalStorage/ISyncLocalStorageService.cs
+++ src/Blazored.LocalStorage/ISyncLocalStorageService.cs
@@ -11,7 +11,7 @@ namespace Blazored.LocalStorage
         string Key(int index);
 
         /// <summary>
-        /// Check if the key exist in the local storage, don't check the value
+        /// Checks if the key exists in local storage but does not check the value.
         /// </summary>
         /// <param name="key">name of the key</param>
         /// <returns>True if the key exist, false otherwise</returns>
