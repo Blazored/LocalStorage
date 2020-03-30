147ad60673afabc8f71a878e5100aadf88c0db61
Chris Sainty <chris@chrissainty.com>
2020-03-30 23:32:18 +0100

    Fixed bug and added some basic unit tests

---
+++ tests/Blazored.LocalStorage.Tests/Mocks/JSRuntimeWrapper.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.JSInterop;
+using Microsoft.JSInterop.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Blazored.LocalStorage.Tests.Mocks
+{
+    public class JSRuntimeWrapper : IJSRuntime
+    {
+        public virtual ValueTask<TValue> InvokeAsync<TValue>(string identifier, object[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual  ValueTask<TValue> InvokeAsync<TValue>(string identifier, CancellationToken cancellationToken, object[] args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
