325ee1a2aa78f6b75770fd3d9a41d4f47393ef50
Chris Sainty <chris@chrissainty.com>
2022-03-28 13:47:20 +0200

    Added ability to register local storage services as singleton

--- samples/BlazorServer/Program.cs
+++ samples/BlazorServer/Program.cs
@@ -1,20 +1,33 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+using Blazored.LocalStorage;
+using Blazored.LocalStorage.Serialization;
+using BlazorServer;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
-namespace BlazorServer
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor(config => config.DetailedErrors = true);
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.Replace(ServiceDescriptor.Scoped<IJsonSerializer, NewtonSoftJsonSerializer>());
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
