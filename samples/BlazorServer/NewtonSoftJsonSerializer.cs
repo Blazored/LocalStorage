1027325ed93ada8db7cce24d7de5bda2d21a511c
Chris Sainty <chrissainty@users.noreply.github.com>
2022-11-09 16:01:30 +0000

    Removes .NET 5 target and adds .NET 7 (#193)
    * Removed .NET 5 target and added .NET 7
    
    * Updated test project to target .NET 7

--- samples/BlazorServer/NewtonSoftJsonSerializer.cs
+++ samples/BlazorServer/NewtonSoftJsonSerializer.cs
@@ -5,7 +5,7 @@ namespace BlazorServer;
 
 public class NewtonSoftJsonSerializer : IJsonSerializer
 {
-    public T Deserialize<T>(string text)
+    public T? Deserialize<T>(string text)
         => JsonConvert.DeserializeObject<T>(text);
 
     public string Serialize<T>(T obj)
--- samples/BlazorServer/Pages/Error.cshtml.cs
+++ samples/BlazorServer/Pages/Error.cshtml.cs
@@ -13,7 +13,7 @@ namespace WebApplication2.Pages
     [IgnoreAntiforgeryToken]
     public class ErrorModel : PageModel
     {
-        public string RequestId { get; set; }
+        public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
