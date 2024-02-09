9b905717df7371b3e799490cad09421bf8aa37f0
Chris Sainty <chris@chrissainty.com>
2024-02-09 23:41:04 +0000

    Added new interactive server project and updated CI bits

---
+++ samples/InteractiveServer/Program.cs
@@ -0,0 +1,34 @@
+using Blazored.LocalStorage;
+using Blazored.LocalStorage.Serialization;
+using BlazorServer;
+using InteractiveServer.Components;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.Replace(ServiceDescriptor.Scoped<IJsonSerializer, NewtonSoftJsonSerializer>());
+builder.Services.AddBlazoredLocalStorage();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
